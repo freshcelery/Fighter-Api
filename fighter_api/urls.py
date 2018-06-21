ecb7d1f5e7883824687fb043dc1a39a3927309cb
Cory Purnell <cpurnell91@gmail.com>
2018-06-20 17:38:49 -0700

    fixed urls

--- fighter_api/urls.py
+++ fighter_api/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^', include('fighters.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
