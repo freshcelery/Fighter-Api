0913c89e3116763c61136a0e07fd5e25a6162aa6
Cory Purnell <cpurnell91@gmail.com>
2018-06-20 17:34:33 -0700

    Added static and updated db settings

--- fighter_api/settings.py
+++ fighter_api/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'fighter_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
