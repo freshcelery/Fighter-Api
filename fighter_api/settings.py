b201e1b8b2eb248733854475c32da08d833a8b22
Cory Purnell <cpurnell91@gmail.com>
2018-06-20 21:37:05 -0700

    fixed staticfiles path

--- fighter_api/settings.py
+++ fighter_api/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
