10159295e01bc1144e37028c2cea70aa5b0184e7
Cory Purnell <cpurnell91@gmail.com>
2018-09-10 11:42:55 -0700

    changed image field to use char

--- fighters/models.py
+++ fighters/models.py
@@ -32,7 +32,7 @@ class Fighter(models.Model):
     )
     latitude = models.FloatField(default=None, blank=True, null=True)
     longitude = models.FloatField(default=None, blank=True, null=True)
-    image = models.URLField(default=None, blank=True, null=True)
+    image = models.CharField(max_length=200)
 
     class Meta:
         ordering = ('name',)
